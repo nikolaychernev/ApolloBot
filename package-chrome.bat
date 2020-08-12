056912fb084806e835afb759ddfc334f977a2d0a
Nikolay Chernev <nikolay.chernev@sirma.bg>
2020-08-12 18:01:33 +0300

    Add custom GitHub page html.

--- package-chrome.bat
+++ package-chrome.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a apollobot-chrome.zip * -x!.git -x!.idea -x!misc -x!firefox-manifest.json -x!package-firefox.bat -x!package-chrome.bat
+"C:\Program Files\7-Zip\7z.exe" a apollobot-chrome.zip * -x!.git -x!.idea -x!misc -x!docs -x!firefox-manifest.json -x!package-firefox.bat -x!package-chrome.bat
 "C:\Program Files\7-Zip\7z.exe" rn apollobot-chrome.zip chrome-manifest.json manifest.json
