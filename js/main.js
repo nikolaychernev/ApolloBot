9f6f1d311b2fab33abe9b0238ccf3ca29408c039
Nikolay Chernev <nikolay.chernev@sirma.bg>
2021-09-10 16:51:02 +0300

    Fix Firefox window.open bug.

--- js/main.js
+++ js/main.js
@@ -353,17 +353,16 @@ function onBuyLicenseBtnClicked() {
         y: (screen.height / 2) - (700 / 2)
     };
 
-    let paymentWindow = window.open('', '_blank',
+    let paymentWindow = window.open('https://apollo-bot.com/creating-payment.html', '_blank',
         `width=${pos.width}, height=${pos.height}, left=${pos.x}, top=${pos.y}`);
-
-    paymentWindow.document.write('Creating payment...');
-    timeoutButton(9, buyLicenseBtn, $(buyLicenseBtn).find('span').text());
 
     makeRequest({
         url: "https://wit6ycuqu7.execute-api.us-east-2.amazonaws.com/default/createOrder"
     }, function (data) {
         paymentWindow.location.href = data.approveUrl;
     });
+
+    timeoutButton(9, buyLicenseBtn, $(buyLicenseBtn).find('span').text());
 }
 
 function hideLicensePage() {
