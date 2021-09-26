bdae29672226b826cb22788fa9d00222321c10e9
Nikolay Chernev <nikolay.chernev@sirma.bg>
2021-09-27 00:35:49 +0300

    Add AWS lambdas.

--- package-firefox.bat
+++ package-firefox.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a apollobot-firefox.zip * -x!.git -x!.idea -x!misc -x!docs -x!chrome-manifest.json -x!package-firefox.bat -x!package-chrome.bat
+"C:\Program Files\7-Zip\7z.exe" a apollobot-firefox.zip * -x!.git -x!.idea -x!lambdas -x!misc -x!chrome-manifest.json -x!package-firefox.bat -x!package-chrome.bat
 "C:\Program Files\7-Zip\7z.exe" rn apollobot-firefox.zip firefox-manifest.json manifest.json
